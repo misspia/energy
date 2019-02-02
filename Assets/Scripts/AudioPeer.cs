bb84a09e4f10d5df3be3f4e307c2dee3fb5775a7
misspia <pia_leung@hotmail.com>
2019-02-01 20:20:10 -0500

    Set up audio context

---
+++ Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class AudioPeer: MonoBehaviour
+{
+    AudioSource audioSource;
+
+    public float[] nodes = new float[512];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource> ();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(nodes, 0, FFTWindow.Blackman);
+    }
+}
