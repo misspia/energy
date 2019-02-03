31efc0389986dcf667714e46b3ed073323692a29
misspia <pia_leung@hotmail.com>
2019-02-02 20:28:19 -0500

    Instantiate audio nodes

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public GameObject nodePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Console.WriteLine("NODE START");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Visualization.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+
+public class Visualization : MonoBehaviour
+{
+    public GameObject audioNodePrefab;
+    GameObject[] audioNodes = new GameObject[512];
+    // Start is called before the first frame update
+    void Start()
+    {
+        InstantiateAudioNodes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void InstantiateAudioNodes()
+    {
+        for(var i = 0; i < 512; i++)
+        {
+            GameObject node = Instantiate(
+                audioNodePrefab,
+                new Vector3(i, 0, 0),
+                Quaternion.identity
+            );
+            node.transform.name = "node" + i;
+            node.GetComponent<Renderer>().material.color = new Color(0.50f, 0.5f, 0.9f, 1.0f);
+        }
+    }
+}
