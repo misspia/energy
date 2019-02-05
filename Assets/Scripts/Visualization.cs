40977cb3f265b63c086e9524283063a0826bc556
misspia <pia_leung@hotmail.com>
2019-02-05 02:08:23 -0500

    Add smoke

--- Assets/Scripts/Visualization.cs
+++ Assets/Scripts/Visualization.cs
@@ -10,7 +10,7 @@ public class Visualization : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InstantiateAudioNodes();
+        //InstantiateAudioNodes();
     }
 
     // Update is called once per frame
